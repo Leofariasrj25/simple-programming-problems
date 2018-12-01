dbf013e3d9bdade84de632c340ac5edf1fd51dca
Leonardo Farias <leofariasrj25@gmail.com>
2018-12-01 14:02:46 -0200

    ex3 elementary solution

---
+++ elementary/python/elitist_hello_world(ex3).py
@@ -0,0 +1,6 @@
+user_name = input("What's your name, human? ")
+
+if user_name.lower() == "Alice".lower() or user_name.lower() == "Bob".lower():
+	print(f"Hello {user_name}, it's nice to see you again!")
+else:
+	print("Hello meatbag, now please, leve me alone.")
